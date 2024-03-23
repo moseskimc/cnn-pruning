c63b688b36b3d0102a96f433ed9eee471b7ed690
Moses Kim <mosesckim@gmail.com>
2024-03-23 23:46:16 +0900

    add config modules

---
+++ src/config/inference_params.py
@@ -0,0 +1,13 @@
+RUN_ID = "af2b277f5148462c8ba9bf1a6c1917e2"
+FMNIST_LABELS = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
---
+++ src/config/train_params.py
@@ -0,0 +1,7 @@
+import torch
+
+
+NUM_EPOCHS = 5
+OPT_FUNC = torch.optim.Adam
+LEARNING_RATE = 1e-3
+BATCH_SIZE = 32
